12d6bced0bb96dd4fd55014ff50b91e937d7e287
Ivan Acosta-Rubio <ivan@Macintosh.local>
2008-07-12 19:51:04 -0400

    unit testing for email

--- test/unit/notifier_test.rb
+++ test/unit/notifier_test.rb
@@ -1,20 +1,13 @@
 require 'test/test_helper'
 require 'notifier'
 
-class NotifierTest < Test::Unit::TestCase
-  CHARSET = "utf-8" 
-
-  
-
-  include ActionMailer::Quoting
+class NotifierTest < Test::Unit::TestCase 
 
   def setup
     ActionMailer::Base.delivery_method = :test
     ActionMailer::Base.perform_deliveries = true
     ActionMailer::Base.deliveries = []
 
-    @expected = TMail::Mail.new
-    @expected.set_content_type "text", "plain", { "charset" => CHARSET }
   end
 
   def test_job_posted
