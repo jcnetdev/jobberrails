00b45fdaa24e3c9dfbd70773b8c7d1ea52c4d753
Jacques Crocker <jcnetdev@gmail.com>
2008-08-08 11:57:55 -0700

    Cleaning up Javascripts and adding Merge script

--- public/javascripts/jquery.ext/jquery.onPage.js
+++ public/javascripts/jquery.ext/jquery.onPage.js
@@ -2,8 +2,8 @@
 // Returns whether or not a result set has results in it
 jQuery.fn.onPage = function() { 
   return this.size() > 0;
-} 
+};
 
 jQuery.fn.notOnPage = function() { 
   return this.size() == 0;
-} 
+};
--- public/javascripts/min/components.js
+++ public/javascripts/min/components.js
@@ -22,6 +22,37 @@ $j(document).ready(function() {
   
 });
 
+/* ---- Compressing ./public/javascripts/components/categories.js ----- */
+$j(document).ready(function() {
+    $j("div.categoryHandle").mousedown(function() {
+        $j(this).fadeTo(500, 0.4).addClass('draggingItem').get(0);
+    });
+    $j("div.categoryHandle").mouseup(function() {
+        $j(this).fadeTo(500,1).removeClass('draggingItem');
+    });
+});
+
+function showOverlay(element) {
+    var el = document.getElementById('overlay');
+    var elTo = document.getElementById(element);
+    if (!elTo) {
+        return;
+    }
+    var sz = elTo.positionedOffset(elTo);
+    
+    if (element != "categoriesContainer") {
+        var tt = document.getElementById("categoriesContainer").positionedOffset(elTo);    
+        el.style.top = tt.top + sz.top + 'px';
+        el.style.left = tt.left + sz.left + 'px';
+    } else {
+        el.style.top = sz.top + 'px';
+        el.style.left = sz.left + 'px';
+    }
+    el.style.width = elTo.offsetWidth + 'px';
+    el.style.height = elTo.offsetHeight + 'px';
+    el.style.display = 'block';
+}
+
 /* ---- Compressing ./public/javascripts/components/flash.js ----- */
 $j(document).ready(function() {
   $j(".flash a.close-text").click(function() {
